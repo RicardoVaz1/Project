638a19195f6c30e49863fdcb072d92a239fa0113
RicardoVaz1 <vaz.ric4rd0@gmail.com>
2022-07-16 18:23:22 +0100

    Add Frontend Routes (Merchant, Admin and Vote)
    Why:
    * To connect Frontend with Smart Contracts
    
    How:
    * Using React Route v6
    * Changing the index.js and App.js
    * Creating a MainHeader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+// Pages
+import MainHeader from "./components/MainHeader";
+import Home from "./pages/Home";
+
+/* ----- Admin Routes ----- */
+import AdminLogin from "./pages/Admin/Login";
+import AdminLogged from "./pages/Admin/Logged";
+import AdminMerchantsList from "./pages/Admin/MerchantsList";
+import AdminEditMerchant from "./pages/Admin/EditMerchant";
+
+/* ----- Merchant Routes ----- */
+import Signup from "./pages/Merchant/Signup";
+import Login from "./pages/Merchant/Login";
+import Logged from "./pages/Merchant/Logged";
+
+
+/* ----- Merchant Routes ----- */
+import Vote from "./pages/Vote/Vote";
+import MerchantInfo from "./pages/Vote/MerchantInfo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MainHeader />
+      <main className="center">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* ----- Admin Routes ----- */}
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route path="/admin-logged" element={<AdminLogged />} />
+          <Route path="/merchants-list" element={<AdminMerchantsList />} />
+          <Route path="/admin-logged/edit/merchant/:id" element={<AdminEditMerchant />} />
+
+
+          {/* ----- Merchant Routes ----- */}
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logged" element={<Logged />} />
+
+
+          {/* ----- Vote Routes ----- */}
+          <Route path="/vote" element={<Vote />} />
+          <Route path="/vote/merchant/:id" element={<MerchantInfo />} />
+        </Routes>
+      </main>
     </div>
   );
 }
