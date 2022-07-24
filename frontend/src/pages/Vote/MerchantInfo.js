c84ef8b689b1e8da2d9b06370b9a2704b1783548
RicardoVaz1 <vaz.ric4rd0@gmail.com>
2022-07-24 23:46:02 +0100

    Fix Vote pages

--- frontend/src/pages/Vote/MerchantInfo.js
+++ frontend/src/pages/Vote/MerchantInfo.js
@@ -1,41 +1,55 @@
-import React from 'react'
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
+import * as constants from '../../constants'
+
+import { ethers } from "ethers"
+import MainContractABI from "../../abis/MainContract.json"
+const ContractAddress = constants.CONTRACTADDRESS
 
 const MerchantInfo = () => {
-    const navigate = useNavigate();
+    const navigate = useNavigate()
 
-    const location = new URL(window.location.href).pathname;
-    const locationArray = location.split("/");
-    const MerchantID = locationArray[3];
+    const location = new URL(window.location.href).pathname
+    const locationArray = location.split("/")
+    const MerchantID = locationArray[3]
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const instanceMainContract = new ethers.Contract(ContractAddress, MainContractABI.abi, signer)
 
     const merchantsList = [
-        { id: 0, walletAddress: "1234", merchantName: "zxc", numberOfVotes: 50 },
-        { id: 1, walletAddress: "1234", merchantName: "zxc", numberOfVotes: 50 },
-        { id: 2, walletAddress: "1234", merchantName: "zxc", numberOfVotes: 500 },
-        { id: 3, walletAddress: "1234", merchantName: "zxc", numberOfVotes: 200 },
-        { id: 4, walletAddress: "1234", merchantName: "zxc", numberOfVotes: 1500 },
+        { id: 0, merchantAddress: "1234", merchantName: "zxc", numberOfVotes: 50 },
+        { id: 1, merchantAddress: "1234", merchantName: "zxc", numberOfVotes: 50 },
+        { id: 2, merchantAddress: "1234", merchantName: "zxc", numberOfVotes: 500 },
+        { id: 3, merchantAddress: "1234", merchantName: "zxc", numberOfVotes: 200 },
+        { id: 4, merchantAddress: "1234", merchantName: "zxc", numberOfVotes: 1500 },
     ]
 
-    function vote(ID) {
+    async function vote(ID) {
         console.log("ID: ", ID)
 
-        document.getElementById("done-successfully").style.display = '';
+        document.getElementById("done-successfully").style.display = ''
 
-        // MainContract > voteNewMerchantContractApproval(ID)
+        try {
+            const userVote = await instanceMainContract.hello() // voteNewMerchantContractApproval(ID).call({from: currentAccount})
+            console.log("User Vote: ", userVote)
+        } catch (error) {
+            console.log("ERROR DURING VOTE: ", error)
+        }
 
         setTimeout(function () {
+            document.getElementById("done-successfully").style.display = 'none'
             navigate("/vote")
-        }, 5000);
+        }, 2000)
     }
 
     return (
-        <div>
+        <>
             <h1>Merchant #{MerchantID}</h1>
 
             <span>ID: {merchantsList[MerchantID].id}</span>
             <br />
 
-            <span>Wallet Address: {merchantsList[MerchantID].walletAddress}</span>
+            <span>Merchant Address: {merchantsList[MerchantID].merchantAddress}</span>
             <br />
 
             <span>Merchant Name: {merchantsList[MerchantID].merchantName}</span>
@@ -49,8 +63,9 @@ const MerchantInfo = () => {
                 <button onClick={() => navigate("/vote")}>Cancel</button>
                 <button onClick={() => vote(MerchantID)}>Vote</button>
             </div>
+
             <span id="done-successfully" style={{ "display": "none" }}>Thank you for your vote! <br /> Redirecting ...</span>
-        </div>
+        </>
     )
 }
 
--- frontend/src/pages/Vote/Vote.js
+++ frontend/src/pages/Vote/Vote.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 
 const Vote = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const merchantsList = [
-    {id: 0, walletAddress: "1234", merchantName: "zxc", numberOfVotes: 50},
-    {id: 1, walletAddress: "1234", merchantName: "zxc", numberOfVotes: 50},
-    {id: 2, walletAddress: "1234", merchantName: "zxc", numberOfVotes: 500},
-    {id: 3, walletAddress: "1234", merchantName: "zxc", numberOfVotes: 200},
-    {id: 4, walletAddress: "1234", merchantName: "zxc", numberOfVotes: 1500},
+    { id: 0, merchantAddress: "1234", merchantName: "zxc", numberOfVotes: 50 },
+    { id: 1, merchantAddress: "1234", merchantName: "zxc", numberOfVotes: 50 },
+    { id: 2, merchantAddress: "1234", merchantName: "zxc", numberOfVotes: 500 },
+    { id: 3, merchantAddress: "1234", merchantName: "zxc", numberOfVotes: 200 },
+    { id: 4, merchantAddress: "1234", merchantName: "zxc", numberOfVotes: 1500 },
   ]
 
   return (
-    <div>
+    <>
       <h1>Vote</h1>
 
       <table className="table">
         <tr>
           <th>ID</th>
-          <th>Wallet Address</th>
+          <th>Merchant Address</th>
           <th>Merchant Name</th>
           <th>No. of Votes</th>
           <th></th>
@@ -29,17 +28,17 @@ const Vote = () => {
           return (
             <tr className="item" key={item.id}>
               <td className="itemDisplay">{item.id}</td>
-              <td className="itemDisplay">{item.walletAddress}</td>
+              <td className="itemDisplay">{item.merchantAddress}</td>
               <td className="itemDisplay">{item.merchantName}</td>
               <td className="itemDisplay">{item.numberOfVotes}</td>
               <td className="removeItemButton">
                 <button onClick={() => navigate(`/vote/merchant/${item.id}`)}>+Info</button>
               </td>
             </tr>
-          );
+          )
         })}
       </table>
-    </div>
+    </>
   )
 }
 
