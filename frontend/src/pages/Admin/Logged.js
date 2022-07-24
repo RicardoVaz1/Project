70d4632b6edd6cb44c1ba04134c1e2aa9709a9cb
RicardoVaz1 <vaz.ric4rd0@gmail.com>
2022-07-24 23:44:01 +0100

    Fix Admin pages

--- frontend/src/pages/Admin/Logged.js
+++ frontend/src/pages/Admin/Logged.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+import Sidebar from '../../components/Sidebar'
+import AddMerchant from './AddMerchant'
 import MerchantsList from './MerchantsList'
-import Sidebar from '../../components/Sidebar';
 
 const Logged = () => {
+    const [searchparams] = useSearchParams()
+    let CurrentAccount = searchparams.get("CurrentAccount")
+    // let Balance = searchparams.get("Balance")
+
     return (
-        <div>
-            <h1>Admin Logged</h1>
+        <>
+            <h1>Welcome Admin, {CurrentAccount.slice(0, 5)}...{CurrentAccount.slice(38)}!</h1>
 
             <Sidebar logged={"admin"} />
 
             <div id="page-wrap">
-                <MerchantsList />
+                <AddMerchant currentAccount={CurrentAccount} />
+                <MerchantsList currentAccount={CurrentAccount} />
             </div>
-        </div>
+
+            <br />
+            <br />
+        </>
     )
 }
 
--- frontend/src/pages/Admin/Login.js
+++ frontend/src/pages/Admin/Login.js
@@ -1,17 +1,11 @@
-import React from 'react'
-// import { useNavigate } from "react-router-dom";
-
 import ConnectWallet from '../../components/ConnectWallet'
 
 const Login = () => {
-  // const navigate = useNavigate();
-
   return (
-    <div>
+    <>
       <h1>Login</h1>
-      <ConnectWallet nextPage="/admin-logged"/>
-      {/* <button onClick={() => navigate("/admin-logged")}>Login</button> */}
-    </div>
+      <ConnectWallet nextPage="/admin-logged" />
+    </>
   )
 }
 
