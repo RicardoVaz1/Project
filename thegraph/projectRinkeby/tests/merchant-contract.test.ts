6749b1233284ca9820b86bfe14dae9a8b1058a37
RicardoVaz1 <vaz.ric4rd0@gmail.com>
2022-08-10 01:18:13 +0100

    ProjectRinkeby Subgraph

---
+++ thegraph/projectRinkeby/tests/merchant-contract.test.ts
@@ -0,0 +1,58 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address, BigInt } from "@graphprotocol/graph-ts"
+import { ApprovedMerchant } from "../generated/schema"
+import { ApprovedMerchant as ApprovedMerchantEvent } from "../generated/MerchantContract/MerchantContract"
+import { handleApprovedMerchant } from "../src/merchant-contract"
+import { createApprovedMerchantEvent } from "./merchant-contract-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let MerchantContractAddress = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let ApprovedMerchant = "boolean Not implemented"
+    let newApprovedMerchantEvent = createApprovedMerchantEvent(
+      MerchantContractAddress,
+      ApprovedMerchant
+    )
+    handleApprovedMerchant(newApprovedMerchantEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("ApprovedMerchant created and stored", () => {
+    assert.entityCount("ApprovedMerchant", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "ApprovedMerchant",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "MerchantContractAddress",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "ApprovedMerchant",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "ApprovedMerchant",
+      "boolean Not implemented"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
